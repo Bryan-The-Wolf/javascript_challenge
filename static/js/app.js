edd4951c855187b135b26e8f0434e54ea1639fd0
Bryan-The-Wolf <66890160+Bryan-The-Wolf@users.noreply.github.com>
2020-12-12 23:13:44 -0600

    adds table append

--- static/js/app.js
+++ static/js/app.js
@@ -8,14 +8,32 @@ let enterButton = d3.select("#filter-btn");
 dateFromPage.on("submit", runEnter);
 enterButton.on("click", runEnter);
 
-//code from class lesson fuction to take info from index
+//code from class lesson 3.9 fuction to take info from index
 function runEnter(){
     d3.event.preventDefault();
     let inputElement = d3.select("#datetime");
     let inputValue = inputElement.property("value");
-    
+
+// testing that the date entered into the form works    
+
     console.log(inputValue)
 
+// now that it works. taking the date and filtering it through the data.js. with this fuction.
+
     let dataFilter = tableData.filter(ufoInfo => ufoInfo.datetime === inputValue);
+
+//now making sure that the array is correct  
+
     console.log(dataFilter);
+
+
+// now I have to put the data from data.js into the html at tbody
+// code from class lesson 3.3
+    const tbody = d3.select("tbody");
+
+    dataFilter.forEach(function(ufoInfo){
+    console.log(ufoInfo);
+    let row = tbody.append("td");
+    });
+
 };
